9e5699925bcbce90b04cd135117be736602264d7
Elyssa Marie Manango <142665230+arrozcaIdo@users.noreply.github.com>
2025-11-09 20:32:40 +0800

    integrated fast blink code with accelerometer motion detection

--- TIKITIKITIK.ino
+++ TIKITIKITIK.ino
@@ -1,9 +1,6 @@
 #define xPin A1
 #define yPin A2
 #define zPin A3
-#define ledPin1 3
-#define ledPin2 4
-#define ledPin3 5
 
 const int samples = 10;
 const int maxVal = 20;
@@ -17,95 +14,10 @@ int buzzer = 10;
 unsigned long startTime;
 boolean movementDetected = false;
 
-void fastBlink() {
-  for (int i = 0; i < 5; ++i) {
-      digitalWrite(relay, LOW);
-      digitalWrite(buzzer, LOW);
-      digitalWrite(ledPin1, LOW);
-      digitalWrite(ledPin2, LOW);
-      digitalWrite(ledPin3, LOW);
-      delay(100);
-      digitalWrite(relay, HIGH);
-      digitalWrite(buzzer, HIGH);
-      digitalWrite(ledPin1, HIGH);
-      digitalWrite(ledPin2, HIGH);
-      digitalWrite(ledPin3, HIGH);
-      delay(100);
-      digitalWrite(relay, LOW);
-      digitalWrite(buzzer, LOW);
-      digitalWrite(ledPin1, LOW);
-      digitalWrite(ledPin2, LOW);
-      digitalWrite(ledPin3, LOW);
-      delay(100);
-      digitalWrite(relay, HIGH);
-      digitalWrite(buzzer, HIGH);
-      digitalWrite(ledPin1, HIGH);
-      digitalWrite(ledPin2, HIGH);
-      digitalWrite(ledPin3, HIGH);
-      delay(100);
-      digitalWrite(relay, LOW);
-      digitalWrite(buzzer, LOW);
-      digitalWrite(ledPin1, LOW);
-      digitalWrite(ledPin2, LOW);
-      digitalWrite(ledPin3, LOW);
-      delay(100);
-      digitalWrite(relay, HIGH);
-      digitalWrite(buzzer, HIGH);
-      digitalWrite(ledPin1, HIGH);
-      digitalWrite(ledPin2, HIGH);
-      digitalWrite(ledPin3, HIGH);
-      delay(100);
-      digitalWrite(relay, LOW);
-      digitalWrite(buzzer, LOW);
-      digitalWrite(ledPin1, LOW);
-      digitalWrite(ledPin2, LOW);
-      digitalWrite(ledPin3, LOW);
-      delay(100);
-      digitalWrite(relay, HIGH);
-      digitalWrite(buzzer, HIGH);
-      digitalWrite(ledPin1, HIGH);
-      digitalWrite(ledPin2, HIGH);
-      digitalWrite(ledPin3, HIGH);
-      delay(100);
-      digitalWrite(relay, LOW);
-      digitalWrite(buzzer, LOW);
-      digitalWrite(ledPin1, LOW);
-      digitalWrite(ledPin2, LOW);
-      digitalWrite(ledPin3, LOW);
-      delay(100);
-      digitalWrite(relay, HIGH);
-      digitalWrite(buzzer, HIGH);
-      digitalWrite(ledPin1, HIGH);
-      digitalWrite(ledPin2, HIGH);
-      digitalWrite(ledPin3, HIGH);
-      delay(900);
-  }
-}
-
-void slowBlink() {
-  for (int i = 0; i < 2; ++i) {
-    digitalWrite(relay, LOW);
-    digitalWrite(buzzer, LOW);
-    digitalWrite(ledPin1, LOW);
-    digitalWrite(ledPin2, LOW);
-    digitalWrite(ledPin3, LOW);
-    delay(700);
-    digitalWrite(relay, HIGH);
-    digitalWrite(buzzer, HIGH);
-    digitalWrite(ledPin1, HIGH);
-    digitalWrite(ledPin2, HIGH);
-    digitalWrite(ledPin3, HIGH);
-    delay(700);
-  }
-}
-
 void setup() {
   Serial.begin(115200);
   pinMode(relay, OUTPUT);
   pinMode(buzzer, OUTPUT);
-  pinMode(ledPin1, OUTPUT);
-  pinMode(ledPin2, OUTPUT);
-  pinMode(ledPin3, OUTPUT);
 
   for (int i = 0; i < samples; i++) {
     xSample += analogRead(xPin);
@@ -117,14 +29,11 @@ void setup() {
   ySample /= samples;
   zSample /= samples;
 
-  digitalWrite(relay, HIGH); 
-  digitalWrite(ledPin1, HIGH);
-  digitalWrite(ledPin2, HIGH);
-  digitalWrite(ledPin3, HIGH);
+  digitalWrite(relay, HIGH);
 }
 
 void loop() {
-  unsigned long currentTime = millis(); 
+  unsigned long currentTime = millis();
 
   int xRaw = 0, yRaw = 0, zRaw = 0;
 
@@ -151,33 +60,61 @@ void loop() {
 
   if (abs(xValue) > maxVal || abs(yValue) > maxVal || abs(zValue) > maxVal) {
     movementDetected = true;
-    startTime = currentTime;
+    startTime = currentTime; 
   } else {
     movementDetected = false;
   }
 
   if (movementDetected) {
     while (currentTime - startTime < 10000) {
-      fastBlink();
-      currentTime = millis(); 
+      digitalWrite(relay, LOW);
+      digitalWrite (buzzer,LOW);
+      delay(100);
+      digitalWrite(relay, HIGH);
+      digitalWrite (buzzer,HIGH);
+      delay(100);
+      digitalWrite(relay, LOW);
+      digitalWrite (buzzer, LOW);
+      delay(100);
+      digitalWrite(relay, HIGH);
+      digitalWrite (buzzer, HIGH);
+      delay(100);
+      digitalWrite(relay, LOW);
+      digitalWrite (buzzer, LOW);
+      delay(100);
+      digitalWrite(relay, HIGH);
+      digitalWrite (buzzer, HIGH);
+      delay(100);
+      digitalWrite(relay, LOW);
+      digitalWrite (buzzer, LOW);
+      delay(100);
+      digitalWrite(relay, HIGH);
+      digitalWrite (buzzer, HIGH);
+      delay(100);
+      digitalWrite(relay, LOW);
+      digitalWrite (buzzer, LOW);
+      delay(100);
+      digitalWrite(relay, HIGH);
+      digitalWrite (buzzer, HIGH);
+      delay(900);
+      currentTime = millis();
     }
 
-    while (currentTime - startTime >= 10000 && currentTime - startTime < 30000) {
-      slowBlink();
+    while (currentTime - startTime >= 10000 && currentTime - startTime < 20000) {
+      digitalWrite(relay, LOW);
+      digitalWrite (buzzer, LOW);
+      delay(700);
+      digitalWrite(relay, HIGH);
+      digitalWrite (buzzer, HIGH);
+      delay(700);
       currentTime = millis();
     }
 
     digitalWrite(relay, HIGH);
     digitalWrite(buzzer, HIGH);
-    digitalWrite(ledPin1, HIGH);
-    digitalWrite(ledPin2, HIGH);
-    digitalWrite(ledPin3, HIGH);
   } else {
 
     digitalWrite(relay, HIGH);
     digitalWrite(buzzer, HIGH);
-    digitalWrite(ledPin1, HIGH);
-    digitalWrite(ledPin2, HIGH);
-    digitalWrite(ledPin3, HIGH);
   }
 }
