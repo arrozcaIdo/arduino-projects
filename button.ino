330f1c360c2b38fa8b5ced78927f03d5af614d87
Elyssa Marie Manango <142665230+arrozcaIdo@users.noreply.github.com>
2025-11-10 18:58:31 +0800

    button powering led

---
+++ button.ino
@@ -0,0 +1,30 @@
+
+int ledPin = 5;
+int buttonApin = 9;
+int buttonBpin = 8;
+int test = 12;
+byte leds = 0;
+
+void setup() 
+{
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonApin, INPUT_PULLUP);  
+  pinMode(buttonBpin, INPUT_PULLUP);  
+  pinMode(test, OUTPUT);
+}
+
+void loop() 
+{
+  if (digitalRead(buttonApin) == LOW)
+  {
+    digitalWrite(ledPin, HIGH);
+  }
+  if (digitalRead(buttonBpin) == LOW)
+  {
+    digitalWrite(ledPin, LOW);
+  }
+
+ digitalWrite(test, HIGH);
+ 
+
+}
